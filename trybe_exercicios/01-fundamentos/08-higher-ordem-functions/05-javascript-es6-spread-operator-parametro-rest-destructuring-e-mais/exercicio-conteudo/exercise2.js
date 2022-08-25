9d07c3b1a06c90eb9d82c7a824c4c76f883d554a
Gabriel <devpalhares@gmail.com>
2022-08-25 15:57:18 -0300

    exercicios do dia

---
+++ trybe_exercicios/01-fundamentos/08-higher-ordem-functions/05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicio-conteudo/exercise2.js
@@ -0,0 +1,19 @@
+const user = {
+  name: 'Maria',
+  age: 21,
+  nationality: 'Brazilian',
+};
+
+const jobInfos = {
+  profession: 'Software engineer',
+  squad: 'Rocket Landing Logic',
+  squadInitials: 'RLL',
+};
+
+const total = { ...user, ...jobInfos };
+
+const { name, age, nationality, profession, squad, squadInitials } = total;
+
+console.log(
+  `Hi, my name is ${name}, I'm ${age} years old and I'm ${nationality}. I work as a ${profession} and my squad is ${squadInitials}-${squad}`
+);
---
+++ trybe_exercicios/01-fundamentos/08-higher-ordem-functions/05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicio-conteudo/exercise7.js
@@ -0,0 +1,6 @@
+const multiply = (number = 1, value = 1) => {
+  return number * value;
+  // Escreva aqui a sua função
+};
+
+console.log(multiply(8));
