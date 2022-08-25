60bc82dc9187b2c035c3f616afb60f5431643832
Gabriel <devpalhares@gmail.com>
2022-08-25 17:52:32 -0300

    exercicios do dia

---
+++ trybe_exercicios/01-fundamentos/08-higher-ordem-functions/05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicio/exercicio6.js
@@ -0,0 +1,7 @@
+const palio = ['Palio', 'Fiat', 2019];
+const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
+const chiron = ['Chiron', 'Bugatti', 2016];
+
+// escreva toObject abaixo
+const trocaObjeto = ([name, brand, year]) => ({ name, brand, year });
+console.log(trocaObjeto(palio));
---
+++ trybe_exercicios/01-fundamentos/08-higher-ordem-functions/05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicio/exercicio7.js
@@ -0,0 +1,27 @@
+const ships = [
+  {
+    name: 'Titanic',
+    length: 269.1,
+    measurementUnit: 'meters',
+  },
+  {
+    name: 'Queen Mary 2',
+    length: 1132,
+    measurementUnit: 'feet',
+  },
+  {
+    name: 'Yamato',
+    length: 256,
+    measurementUnit: 'meters',
+  },
+];
+// measurementUnit: unidade de medida
+
+const shipLength = ({ name, age, measurementUnit }) => {
+  return `${name} is ${measurementUnit} meters long`;
+};
+// escreva shipLength abaixo
+
+console.log(shipLength(ships[0])); // 'Titanic is 269.1 meters long'
+console.log(shipLength(ships[1])); // 'Queen Mary 2 is 1132 feet long'
+console.log(shipLength(ships[2])); // 'Yamato is 256 meters long'
