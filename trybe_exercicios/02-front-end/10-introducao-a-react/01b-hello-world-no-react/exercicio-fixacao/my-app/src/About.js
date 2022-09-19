35beee1183a5ef95bad86d26dd85af94a6d72dd5
Gabriel <devpalhares@gmail.com>
2022-09-19 16:51:04 -0300

    exercicio fixacao

---
+++ trybe_exercicios/02-front-end/10-introducao-a-react/01b-hello-world-no-react/exercicio-fixacao/my-app/src/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class About extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Gabriel</h1>
+        <p>tenho 23 anos nascido em Patos de Minas</p>
+        <h2>Minhas Habilidades</h2>
+        <ul>
+          <li>Javascript</li>
+          <li>React</li>
+          <li>Git</li>
+        </ul>
+      </>
+    );
+  }
+}
+
+export default About;
