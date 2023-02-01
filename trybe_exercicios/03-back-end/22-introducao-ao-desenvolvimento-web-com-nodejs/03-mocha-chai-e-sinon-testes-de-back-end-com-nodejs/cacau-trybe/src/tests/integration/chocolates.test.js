5ef0e8599e4b5883043877c5242b2ec145f8efa3
Gabriel <devpalhares@gmail.com>
2023-02-01 15:54:44 -0300

    criação dos testes exercicios fixaxao

---
+++ trybe_exercicios/03-back-end/22-introducao-ao-desenvolvimento-web-com-nodejs/03-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/cacau-trybe/src/tests/integration/chocolates.test.js
@@ -0,0 +1,27 @@
+// tests/integration/chocolates.test.js
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const { expect } = chai;
+
+chai.use(chaiHttp);
+
+describe('Testando a API Cacau Trybe', function () {
+  describe('Usando o método GET em /chocolates', function () {
+    it('Retorna a lista completa de chocolates!', async function () {
+      const output = [
+        { id: 1, name: 'Mint Intense', brandId: 1 },
+        { id: 2, name: 'White Coconut', brandId: 1 },
+        { id: 3, name: 'Mon Chéri', brandId: 2 },
+        { id: 4, name: 'Mounds', brandId: 3 },
+      ];
+
+      const response = await chai
+        .request(app)
+        .get('/chocolates');
+      expect(response.status).to.be.equal(200);
+      expect(response.body.chocolates).to.deep.equal(output);
+    });
+  });
+});
