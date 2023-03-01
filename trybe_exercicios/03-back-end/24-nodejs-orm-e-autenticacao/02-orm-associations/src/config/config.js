a593346b186c44b69430fc2ec19e23d888d3b4f6
Gabriel <devpalhares@gmail.com>
2023-03-01 15:31:44 -0300

    config.js

--- trybe_exercicios/03-back-end/24-nodejs-orm-e-autenticacao/02-orm-associations/src/config/config.js
+++ trybe_exercicios/03-back-end/24-nodejs-orm-e-autenticacao/02-orm-associations/src/config/config.js
@@ -2,21 +2,21 @@
   "development": {
     "username": "root",
     "password": null,
-    "database": "database_development",
+    "database": "books_database_development",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
     "username": "root",
     "password": null,
-    "database": "database_test",
+    "database": "books_database_test",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
     "password": null,
-    "database": "database_production",
+    "database": "books_database_production",
     "host": "127.0.0.1",
     "dialect": "mysql"
   }
