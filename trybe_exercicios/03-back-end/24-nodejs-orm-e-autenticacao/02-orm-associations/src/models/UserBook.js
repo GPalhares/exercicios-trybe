5c856e9584cdc42978f69bb3e663614d0c1a5648
Gabriel <devpalhares@gmail.com>
2023-03-01 15:45:18 -0300

    associate1

--- trybe_exercicios/03-back-end/24-nodejs-orm-e-autenticacao/02-orm-associations/src/models/UserBook.js
+++ trybe_exercicios/03-back-end/24-nodejs-orm-e-autenticacao/02-orm-associations/src/models/UserBook.js
@@ -8,6 +8,21 @@ module.exports = (sequelize, _DataTypes) => {
         tableName: 'users_books',
       },
     );
+
+    UserBook.associate = (models) => {
+        models.Book.belongsToMany(models.User, {
+          as: 'users',
+          through: UserBook,
+          foreignKey: 'bookId', // se refere ao id de Book na tabela de `users_books`
+          otherKey: 'userId', // se refere a outra chave de `users_books`
+        });
+        // models.User.belongsToMany(models.Book, {
+        //   as: 'books',
+        //   through: UserBook,
+        //   foreignKey: 'userId', // se refere ao id de User na tabela de `users_books`
+        //   otherKey: 'bookId',
+        // });
+      };
   
     return UserBook;
   };
