ca7637070a8bc08e31cc4911400086e4f000dfe7
Gabriel <devpalhares@gmail.com>
2023-03-09 15:34:35 -0300

    exercise 4

--- trybe_exercicios/03-back-end/26-typescript/01a-introducao-typescript/exercises/exercises.ts
+++ trybe_exercicios/03-back-end/26-typescript/01a-introducao-typescript/exercises/exercises.ts
@@ -9,3 +9,7 @@ export function getRectangleArea(base: number, height: number): number {
 export function getTriangleArea(base: number, height: number): number {
   return (base * height) / 2;
 }
+
+export function getPolygonPerimeter(sides: number[]): number {
+  return sides.reduce((acc, side) => acc + side, 0);
+}
