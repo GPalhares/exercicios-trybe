20ebf322f8b30d46eb479505706f8339f7c44f16
Gabriel <devpalhares@gmail.com>
2023-03-09 15:35:01 -0300

    exercise 5

--- trybe_exercicios/03-back-end/26-typescript/01a-introducao-typescript/exercises/exercises.ts
+++ trybe_exercicios/03-back-end/26-typescript/01a-introducao-typescript/exercises/exercises.ts
@@ -13,3 +13,14 @@ export function getTriangleArea(base: number, height: number): number {
 export function getPolygonPerimeter(sides: number[]): number {
   return sides.reduce((acc, side) => acc + side, 0);
 }
+
+export function triangleCheck(
+  sideA: number,
+  sideB: number,
+  sideC: number
+): boolean {
+  const checkSideA = sideB - sideC < sideA && sideA < sideB + sideC;
+  const checkSideB = sideA - sideC < sideB && sideB < sideA + sideC;
+  const checkSideC = sideA - sideB < sideC && sideC < sideA + sideB;
+  return checkSideA && checkSideB && checkSideC;
+}
