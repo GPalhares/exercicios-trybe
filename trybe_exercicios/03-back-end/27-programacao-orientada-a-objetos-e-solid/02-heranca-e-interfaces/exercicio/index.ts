f90ddc401bf5aa9e200f59d6b45f7318488b8e04
Gabriel <devpalhares@gmail.com>
2023-03-20 18:01:02 -0300

    index.ts for test Employee.ts

--- trybe_exercicios/03-back-end/27-programacao-orientada-a-objetos-e-solid/02-heranca-e-interfaces/exercicio/index.ts
+++ trybe_exercicios/03-back-end/27-programacao-orientada-a-objetos-e-solid/02-heranca-e-interfaces/exercicio/index.ts
@@ -1,21 +1,18 @@
-import Student from './Student';
+import Employee from './Employee';
 
-const carolina = new Student('Carolina da Silva', new Date('2005/03/17'));
-const lucas = new Student('Lucas Peixoto Salgueiro', new Date('2006/07/19'));
-const jessica = new Student('Jéssica Bianca Nunes', new Date('2004/06/06'));
-const tamires = new Student('Tamires Santos Bastos', new Date('2005/11/05'));
-const fernando = new Student('Fernando Gonçalves', new Date('2006/09/11'));
+const testInterfaceEmployee: Employee = {
+  registration: 'FNC1234567891011',
+  salary: 1200.0,
+  admissionDate: new Date(),
 
-carolina.examsGrades = [25, 20, 25, 23];
-lucas.examsGrades = [25, 20, 25, 23];
-jessica.assignmentsGrades = [50, 45];
-tamires.assignmentsGrades = [47, 42];
+  generateRegistration(): string {
+    const randomStr = String(Date.now() * (Math.random() + 1)).replace(
+      /\W/g,
+      ''
+    );
 
-console.log(carolina);
-console.log(lucas);
-console.log(jessica);
-console.log(tamires);
-console.log(fernando);
+    return `FNC${randomStr}`;
+  },
+};
 
-// deve retornar erro
-// fernando.assignmentsGrades = [45, 45, 45];
+console.log(testInterfaceEmployee);
