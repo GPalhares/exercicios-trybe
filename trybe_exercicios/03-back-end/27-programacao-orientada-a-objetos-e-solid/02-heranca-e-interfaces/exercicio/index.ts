6b188a2bdd47f26a62535fd78449bb202f588eb6
Gabriel <devpalhares@gmail.com>
2023-03-20 18:02:51 -0300

    index.ts for test Teacher.ts

--- trybe_exercicios/03-back-end/27-programacao-orientada-a-objetos-e-solid/02-heranca-e-interfaces/exercicio/index.ts
+++ trybe_exercicios/03-back-end/27-programacao-orientada-a-objetos-e-solid/02-heranca-e-interfaces/exercicio/index.ts
@@ -1,19 +1,33 @@
-export default class Subject {
-  constructor(private _name: string) {
-    this.name = _name;
-  }
+import Subject from './Subject';
+import Teacher from './Teacher';
 
-  get name(): string {
-    return this._name;
-  }
+const math = new Subject('Matemática');
+const history = new Subject('História');
+const philosophy = new Subject('Filosofia');
 
-  set name(value: string) {
-    this.validateName(value);
-    this._name = value;
-  }
+const marta = new Teacher('Marta da Silva', new Date('1980/03/30'), 2000, math);
+const joao = new Teacher(
+  'João Antônio da Costa',
+  new Date('1982/04/21'),
+  2000,
+  history
+);
+const lucio = new Teacher(
+  'Lucio Teixeira',
+  new Date('1986/01/29'),
+  2000,
+  philosophy
+);
 
-  private validateName(value: string): void {
-    if (value.length < 3)
-      throw new Error('O nome deve conter no mínimo 3 caracteres.');
-  }
-}
+console.log(marta);
+console.log(joao);
+console.log(lucio);
+
+// deve retornar erro
+//const invalidTeacherSalary = new Teacher('Marta da Silva', new Date('1980/03/30'), 2000, math);
+
+// deve retornar erro
+// const today = new Date();
+// today.setDate(today.getDate() + 1)
+// const tomorrow = today;
+// marta.admissionDate = tomorrow;
